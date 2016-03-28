5407697437f75304cb5aff552d0c99f710a5ceeb
Sergey <Sergey>
2016-03-28 07:25:05 +0300

    Adaptive

--- src/js/partials/helper.js
+++ src/js/partials/helper.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 
   $('.header-menu__menu').on('click', function(e){
     e.preventDefault();
-    $('body').toggleClass('normal small');
+    // $('body').toggleClass('normal small-menu');
+        $('normal').hide().addClass('small-menu')
   });
 
 });
